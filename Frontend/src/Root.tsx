26ebc1a809d348225f0ee8446a91b44e6c765b65
relatablepradeep <relatablepradeep@gmail.com>
2025-09-22 09:07:11 +0530

    Revert "Fix UI/UX and improve responsiveness"

--- Frontend/src/Root.tsx
+++ Frontend/src/Root.tsx
@@ -18,9 +18,7 @@ export default function Root() {
         <Chatbot />
         <Translate />
         <ScrollToTop/>
-        <main className="flex-grow container mx-auto px-4 sm:px-6 lg:px-8 py-8">
-          <Outlet />
-        </main>
+        <Outlet />
         <ScrollToTopButton />
         <Foot />
     
